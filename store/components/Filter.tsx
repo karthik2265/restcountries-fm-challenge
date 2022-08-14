6f0c7a6f45fe98c9c17217dcb1a5517a1742b327
karthik2265 <karthiksurya2022@gmail.com>
2022-08-14 19:12:25 +0530

    filter cmp

---
+++ store/components/Filter.tsx
@@ -0,0 +1,60 @@
+import React, { useRef } from "react";
+import "./Filter.scss";
+import { useDispatch } from "react-redux";
+import { countriesActions } from "../../store/countries-slice";
+
+const Filter = () => {
+  const dispatch = useDispatch();
+
+  const inputRef = useRef<HTMLInputElement>(null);
+  const selectRef = useRef<HTMLSelectElement>(null);
+
+  // handlers
+  function inputCHangeHandler() {
+    if (inputRef.current !== null) {
+      const newSearchTerm = inputRef.current.value;
+      dispatch(countriesActions.updateSearchTerm(newSearchTerm));
+    }
+  }
+
+  function selectCHangeHandler() {
+    if (selectRef.current !== null) {
+      const newSelectedRegion: string | null = selectRef.current.value;
+      dispatch(countriesActions.updateSelectedRegion(newSelectedRegion));
+    }
+  }
+
+  return (
+    <div className="filter">
+      <form className="form__input">
+        <input
+          type="search"
+          name="search"
+          id="search"
+          placeholder="Search for a country"
+          ref={inputRef}
+          onChange={inputCHangeHandler}
+        />
+      </form>
+
+      <div className="filter__regions">
+        <select
+          name="select"
+          id="select"
+          className="select"
+          ref={selectRef}
+          onChange={selectCHangeHandler}
+        >
+          <option value="Filter by region">Filter by region</option>
+          <option value="Africa">Africa</option>
+          <option value="America">America</option>
+          <option value="Asia">Asia</option>
+          <option value="Europe">Europe</option>
+          <option value="Oceania">Oceania</option>
+        </select>
+      </div>
+    </div>
+  );
+};
+
+export { Filter };
