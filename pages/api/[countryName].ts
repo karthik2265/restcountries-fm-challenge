153a60b4fb3dfc2b63ab14839e25e24adc60b14f
karthik2265 <karthiksurya2022@gmail.com>
2022-08-15 20:47:13 +0530

    fetch country details

--- pages/api/[countryName].ts
+++ pages/api/[countryName].ts
@@ -1,8 +1,11 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from "next";
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  
+  const { countryName } = req.query;
+  const response = await fetch(`http://restcountries.com/v3.1/name/${countryName}`);
+  const data = await response.json();
+  return res.send(data);
 }
--- pages/countries/[countryName].tsx
+++ pages/countries/[countryName].tsx
@@ -1,26 +1,32 @@
 import { NextPage } from "next";
 import React from "react";
 import { server } from "../../config/index";
+import { Countries } from "../../types";
 
-const DetailsPage: NextPage = () => {
+const DetailsPage: NextPage<{ countryDetails: any }> = ({ countryDetails }) => {
   return <div>DetailsPage</div>;
 };
 
 export async function getStaticPaths() {
-  // const response = await fetch(`${server}/api/countries`);
-  // const data = await response.json();
+  const response = await fetch(`${server}/api/countries`);
+  const countries: Countries = await response.json();
+  const paths = countries.map((country) => {
+    return {
+      params: { countryName: country.name },
+    };
+  });
   return {
-    fallback: false,
-    paths: [],
+    fallback: true,
+    paths,
   };
 }
 
 export async function getStaticProps(context: any) {
   const countryName = context.params.countryName;
-  // const response = await fetch(`${server}/api/${countryName}`);
-  // const data = await response.json();
+  const response = await fetch(`${server}/api/${countryName}`);
+  const data = await response.json();
   return {
-    props: {},
+    props: { countryDetails: data },
   };
 }
 
