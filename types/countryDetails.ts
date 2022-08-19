8e016ec9aebe313e56c9768c661872af178c2283
karthik2265 <karthiksurya2022@gmail.com>
2022-08-20 01:17:28 +0530

    CountryDetails type

---
+++ types/countryDetails.ts
@@ -0,0 +1,12 @@
+export interface CountryDetails {
+  flag: string;
+  nativeName: string[];
+  population: string;
+  region: string;
+  subRegion: string;
+  capital: string[];
+  topLevelDomain: string[];
+  currencies: string[];
+  languages: string[];
+  borderCountries: string[];
+}
