69065c1c6a6c2ed8afe4023c9ce1d1631e52658b
karthik2265 <karthiksurya2022@gmail.com>
2022-08-21 11:46:58 +0530

    cleanup

--- pages/api/[countryName].ts
+++ pages/api/[countryName].ts
@@ -11,33 +11,6 @@ export default async function handler(
   );
   const data = await response.json();
   const countryData = data[0];
-  // utility func
-  function getValuesOfObjectAsArray(obj: any) {
-    const arr = [];
-    for (const key in obj) {
-      const val = obj[key];
-      arr.push(val);
-    }
-    return arr;
-  }
-  // get names of border countries
-  async function getCommonNamesOfBorderCountriesFromCodeNames(arr: any) {
-    // use promise.all to fetch all
-    let allRequests: Promise<any>[] = [];
-    arr.forEach((code: String) => {
-      allRequests.push(fetch(`https://restcountries.com/v3.1/alpha/${code}`));
-    });
-    const allResponses = await Promise.all(allRequests);
-    allRequests = [];
-    allResponses.forEach((response) => {
-      allRequests.push(response.json());
-    });
-    let allData = await Promise.all(allRequests);
-    allData = allData.map((arr) => arr[0]);
-    const names = allData.map((country) => country.name.common);
-    return names;
-  }
-
   const countryDetails: CountryDetails = {
     flag: countryData.flags.svg,
     nativeName: getValuesOfObjectAsArray(countryData.name.nativeName).map(
@@ -57,4 +30,32 @@ export default async function handler(
     ),
   };
   return res.send(countryDetails);
+}
+
+// utility func
+function getValuesOfObjectAsArray(obj: any) {
+  const arr = [];
+  for (const key in obj) {
+    const val = obj[key];
+    arr.push(val);
+  }
+  return arr;
+}
+
+// get names of border countries
+async function getCommonNamesOfBorderCountriesFromCodeNames(arr: any) {
+  // use promise.all to fetch all
+  let allRequests: Promise<any>[] = [];
+  arr.forEach((code: String) => {
+    allRequests.push(fetch(`https://restcountries.com/v3.1/alpha/${code}`));
+  });
+  const allResponses = await Promise.all(allRequests);
+  allRequests = [];
+  allResponses.forEach((response) => {
+    allRequests.push(response.json());
+  });
+  let allData = await Promise.all(allRequests);
+  allData = allData.map((arr) => arr[0]);
+  const names = allData.map((country) => country.name.common);
+  return names;
 }
