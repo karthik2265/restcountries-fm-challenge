2d59d815da8c11d3b04811d20e270f9940cbdc95
karthik2265 <karthiksurya2022@gmail.com>
2022-08-21 21:39:03 +0530

    uiSlice

--- store/index.ts
+++ store/index.ts
@@ -1,11 +1,14 @@
-import { configureStore } from '@reduxjs/toolkit'
-import { countriesReducer } from './countries-slice'
+import { configureStore } from "@reduxjs/toolkit";
+import { countriesReducer } from "./countries-slice";
+import { uiReducer } from "./ui-slice";
 
-const store = configureStore({ reducer: { countries: countriesReducer } })
+const store = configureStore({
+  reducer: { countries: countriesReducer, ui: uiReducer },
+});
 
-export default store
+export default store;
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
-export type RootState = ReturnType<typeof store.getState>
+export type RootState = ReturnType<typeof store.getState>;
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
-export type AppDispatch = typeof store.dispatch
+export type AppDispatch = typeof store.dispatch;
---
+++ store/ui-slice.ts
@@ -0,0 +1,36 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+interface IState {
+  isLoading: boolean;
+  isLightTheme: boolean;
+}
+
+interface IUpdateAction {
+  payload: boolean;
+}
+
+const initialState: IState = {
+  isLoading: false,
+  isLightTheme: true,
+};
+
+const uiSlice = createSlice({
+  name: "ui",
+  initialState,
+  reducers: {
+    setLoading(state: IState, action: IUpdateAction) {
+      const isLoading = action.payload;
+      state.isLoading = isLoading;
+    },
+    setLightTheme(state: IState, action: IUpdateAction) {
+      const isLightTheme = action.payload;
+      state.isLightTheme = isLightTheme;
+    },
+  },
+});
+
+const uiActions = uiSlice.actions;
+const uiReducer = uiSlice.reducer;
+
+export { uiActions, uiReducer };
+export default uiSlice;
