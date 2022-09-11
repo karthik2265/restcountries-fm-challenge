20db16f097e652f879300e245cd39b1aa13e3e4e
karthik2265 <karthiksurya2022@gmail.com>
2022-09-11 17:16:11 +0530

    add header

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,8 +12,12 @@ import { Filter } from "../components";
 import { server } from "../config/index";
 // head
 import Head from "next/head";
-// link
+// next
 import Link from "next/link";
+import Image from "next/image";
+// images
+import MoonIcon from "../public/images/moon-icon.svg";
+import SunIcon from "../public/images/day-sunny-icon.svg";
 
 const Home: NextPage<{ countries: Countries }> = ({ countries }) => {
   const dispatch = useDispatch();
@@ -22,6 +26,9 @@ const Home: NextPage<{ countries: Countries }> = ({ countries }) => {
   useEffect(() => {
     dispatch(countriesActions.setCountries(countries));
   }, [dispatch, countries]);
+
+  // get theme
+  const isLightTheme = useSelector((state: RootState) => state.ui.isLightTheme);
 
   // get filtered countries from redux store
   const filteredCountries = useSelector(
@@ -44,21 +51,34 @@ const Home: NextPage<{ countries: Countries }> = ({ countries }) => {
         />
         <meta name="supported-color-schemes" content="light dark" />
       </Head>
-      <Filter />
-      {filteredCountries.map((country) => {
-        return (
-          <Link
-            key={country.name}
-            href={`/countries/${country.name}`}
-            passHref={true}
-          >
-            <div style={{ border: "1px solid red" }}>
-              <h1>{country.name}</h1>
-              <h2>{country.region}</h2>
-            </div>
-          </Link>
-        );
-      })}
+      <header>
+        <h1>Where in the world?</h1>
+        <div>
+          {isLightTheme ? (
+            <Image src={MoonIcon} alt="moon-icon" />
+          ) : (
+            <Image src={SunIcon} alt="sun-icon" />
+          )}
+          <h4> {(isLightTheme ? "Dark" : "Light") + " " + "Mode"} </h4>
+        </div>
+      </header>
+      <main>
+        <Filter />
+        {filteredCountries.map((country) => {
+          return (
+            <Link
+              key={country.name}
+              href={`/countries/${country.name}`}
+              passHref={true}
+            >
+              <div style={{ border: "1px solid red" }}>
+                <h1>{country.name}</h1>
+                <h2>{country.region}</h2>
+              </div>
+            </Link>
+          );
+        })}
+      </main>
     </div>
   );
 };
