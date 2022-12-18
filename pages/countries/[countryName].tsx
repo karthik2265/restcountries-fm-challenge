560da02e275b891ab9d8b58742ff8ef1911f1f69
karthik2265 <63234167+karthik2265@users.noreply.github.com>
2022-12-18 19:23:38 +0530

    fix type issue

--- pages/countries/[countryName].tsx
+++ pages/countries/[countryName].tsx
@@ -24,7 +24,7 @@ const DetailsPage: NextPage<{ country: CountryDetails }> = ({ country }) => {
   const router = useRouter();
 
   // event handlers
-  function routeChangeHandler(path: "string") {
+  function routeChangeHandler(path: string) {
     router.push(path);
   }
 
